ec6787ea0a04f135bb217bf2e8cf6e56e3bc409c
Alexander Kosarigin <skosarigin@gmail.com>
2023-09-16 19:58:58 +0300

    01 - add full Interface

--- Homework/HM_01_Nim_game.cpp
+++ Homework/HM_01_Nim_game.cpp
@@ -1,23 +1,26 @@
 #include <std_lib_facilities.h>
 
 void Interface(int n_strings, vector<int> n_rocks) {
-    std::cout <<     "String's number                                Rock's number" << std::endl;
-
+    for (int i = 0; i < 4; ++i) {
+        std::cout << std::endl;
+    }
+    std::cout << "============================================================" << std::endl;
+    std::cout << "String's number                                Rock's number" << std::endl;
     for (int i = 0; i < n_strings; ++i) {
-        std::cout << "  " 
-                        <<i+1<<"||  [";
+        std::cout << " " <<i+1<<"|  [";
         for (int k = 0; k < n_rocks[i]; ++k ) {
             std::cout << " " << "* ";
         }
         for (int k = 0; k < 3*(15 - n_rocks[i]); ++k ) {
             std::cout << " ";
         }
-        std::cout << " ||" << n_rocks[i] << std::endl;
+        std::cout << " |" << n_rocks[i] << std::endl;
     }
-    std::cout <<     "============================================================" << std::endl;
-    std::cout <<     "----------Enter string and rocks number with space----------" << std::endl;
-
+    std::cout << "============================================================" << std::endl;
+    std::cout << "-------Enter string and rocks(<16) number with space--------" << std::endl;
 }
+
+//vector<int> Opponent() {}
 
 int main() {
     vector <int> Rocks_num;
